08b79757370549378ec62abbdd66f1eec8e618eb
Chris Kirby <chris@infinitusdev.com>
2017-01-11 11:26:24 -0600

    added support for Raygun custom group key
    - new message group models for passing the group key to the custom event delegate
    - sink modified with new event, event trigger, and raygun grouping even subscription when custom key is defined
    - extension modified to support new group key property argument

---
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/IMessageGroup.cs
@@ -0,0 +1,15 @@
+﻿namespace Serilog.Sinks.Raygun
+{
+  /// <summary>
+  /// IMessageGroup interface utilized for Raygun custom grouping feature
+  /// </summary>
+  public interface IMessageGroup
+  {
+    /// <summary>
+    /// The custom group key
+    /// </summary>
+    /// <value>The group key.</value>
+    string GroupKey { get; set; }
+
+  }
+}
---
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/MessageGroup.cs
@@ -0,0 +1,23 @@
+﻿namespace Serilog.Sinks.Raygun
+{
+  /// <summary>
+  /// Class MessageGroup.
+  /// </summary>
+  /// <seealso cref="Serilog.Sinks.Raygun.IMessageGroup" />
+  public class MessageGroup : IMessageGroup
+  {
+    /// <summary>
+    /// The custom group key
+    /// </summary>
+    /// <value>The group key.</value>
+    public string GroupKey { get; set; }
+
+    /// <summary>
+    /// Defaults group key to empty
+    /// </summary>
+    public MessageGroup()
+    {
+      GroupKey = string.Empty;
+    }
+  }
+}
