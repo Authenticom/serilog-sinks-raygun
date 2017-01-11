b30e745543a7864e0d5b50472782b8f3722a3c55
Chris Kirby <chris@infinitusdev.com>
2017-01-11 16:02:07 -0600

    moved to 4 space tabs to match upstream repo

--- src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunPropertyFormatter.cs
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunPropertyFormatter.cs
@@ -20,13 +20,13 @@ using Serilog.Events;
 
 namespace Serilog.Sinks.Raygun
 {
-  /// <summary>
-  /// Converts <see cref="LogEventProperty"/> values into simple scalars,
-  /// that render well in Raygun.
-  /// </summary>
-  static class RaygunPropertyFormatter
-  {
-    static readonly HashSet<Type> RaygunScalars = new HashSet<Type>
+    /// <summary>
+    /// Converts <see cref="LogEventProperty"/> values into simple scalars,
+    /// that render well in Raygun.
+    /// </summary>
+    static class RaygunPropertyFormatter
+    {
+        static readonly HashSet<Type> RaygunScalars = new HashSet<Type>
         {
             typeof(bool),
             typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint),
@@ -34,64 +34,64 @@ namespace Serilog.Sinks.Raygun
             typeof(byte[])
         };
 
-    /// <summary>
-    /// Simplify the object so as to make handling the serialized
-    /// representation easier.
-    /// </summary>
-    /// <param name="value">The value to simplify (possibly null).</param>
-    /// <returns>A simplified representation.</returns>
-    public static object Simplify(LogEventPropertyValue value)
-    {
-      var scalar = value as ScalarValue;
-      if (scalar != null)
-        return SimplifyScalar(scalar.Value);
-
-      var dict = value as DictionaryValue;
-      if (dict != null)
-      {
-        var result = new Dictionary<object, object>();
-        foreach (var element in dict.Elements)
+        /// <summary>
+        /// Simplify the object so as to make handling the serialized
+        /// representation easier.
+        /// </summary>
+        /// <param name="value">The value to simplify (possibly null).</param>
+        /// <returns>A simplified representation.</returns>
+        public static object Simplify(LogEventPropertyValue value)
         {
-          var key = SimplifyScalar(element.Key.Value);
-          if (result.ContainsKey(key))
-          {
-            SelfLog.WriteLine("The key {0} is not unique in the provided dictionary after simplification to {1}.", element.Key, key);
-            return dict.Elements.Select(e => new Dictionary<string, object>
+            var scalar = value as ScalarValue;
+            if (scalar != null)
+                return SimplifyScalar(scalar.Value);
+
+            var dict = value as DictionaryValue;
+            if (dict != null)
+            {
+                var result = new Dictionary<object, object>();
+                foreach (var element in dict.Elements)
+                {
+                    var key = SimplifyScalar(element.Key.Value);
+                    if (result.ContainsKey(key))
+                    {
+                        SelfLog.WriteLine("The key {0} is not unique in the provided dictionary after simplification to {1}.", element.Key, key);
+                        return dict.Elements.Select(e => new Dictionary<string, object>
                         {
                             { "Key", SimplifyScalar(e.Key.Value) },
                             { "Value", Simplify(e.Value) }
                         })
-            .ToArray();
-          }
-          result.Add(key, Simplify(element.Value));
+                        .ToArray();
+                    }
+                    result.Add(key, Simplify(element.Value));
+                }
+                return result;
+            }
+
+            var seq = value as SequenceValue;
+            if (seq != null)
+                return seq.Elements.Select(Simplify).ToArray();
+
+            var str = value as StructureValue;
+            if (str != null)
+            {
+                var props = str.Properties.ToDictionary(p => p.Name, p => Simplify(p.Value));
+                if (str.TypeTag != null)
+                    props["$typeTag"] = str.TypeTag;
+                return props;
+            }
+
+            return null;
         }
-        return result;
-      }
 
-      var seq = value as SequenceValue;
-      if (seq != null)
-        return seq.Elements.Select(Simplify).ToArray();
+        static object SimplifyScalar(object value)
+        {
+            if (value == null) return null;
 
-      var str = value as StructureValue;
-      if (str != null)
-      {
-        var props = str.Properties.ToDictionary(p => p.Name, p => Simplify(p.Value));
-        if (str.TypeTag != null)
-          props["$typeTag"] = str.TypeTag;
-        return props;
-      }
+            var valueType = value.GetType();
+            if (RaygunScalars.Contains(valueType)) return value;
 
-      return null;
+            return value.ToString();
+        }
     }
-
-    static object SimplifyScalar(object value)
-    {
-      if (value == null) return null;
-
-      var valueType = value.GetType();
-      if (RaygunScalars.Contains(valueType)) return value;
-
-      return value.ToString();
-    }
-  }
 }
--- src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunSink.cs
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunSink.cs
@@ -24,144 +24,144 @@ using Serilog.Events;
 
 namespace Serilog.Sinks.Raygun
 {
-  /// <summary>
-  /// Writes log events to the Raygun.com service.
-  /// </summary>
-  public class RaygunSink : ILogEventSink
-  {
-    readonly IFormatProvider _formatProvider;
-    readonly string _userNameProperty;
-    readonly string _applicationVersionProperty;
-    readonly IEnumerable<string> _tags;
-    readonly IEnumerable<string> _ignoredFormFieldNames;
-    readonly string _groupKeyProperty;
-    readonly RaygunClient _client;
-
     /// <summary>
-    /// Grouping event delegate
+    /// Writes log events to the Raygun.com service.
     /// </summary>
-    /// <param name="sender">The sender.</param>
-    /// <param name="e">The <see cref="RaygunCustomGroupingKeyEventArgs"/> instance containing the event data.</param>
-    /// <param name="group">The custom group object containing the custom group key</param>
-    public delegate void Grouped(object sender, RaygunCustomGroupingKeyEventArgs e, IMessageGroup group);
-
-    /// <summary>
-    /// Occurs when the Raygun client fires the CustomGroupKey event.
-    /// </summary>
-    public event Grouped Grouping;
-
-    /// <summary>
-    /// Construct a sink that saves errors to the Raygun.io service. Properties are being send as userdata and the level is included as tag. The message is included inside the userdata.
-    /// </summary>
-    /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
-    /// <param name="applicationKey">The application key as found on the Raygun website.</param>
-    /// <param name="wrapperExceptions">If you have common outer exceptions that wrap a valuable inner exception which you'd prefer to group by, you can specify these by providing a list.</param>
-    /// <param name="userNameProperty">Specifies the property name to read the username from. By default it is UserName. Set to null if you do not want to use this feature.</param>
-    /// <param name="applicationVersionProperty">Specifies the property to use to retrieve the application version from. You can use an enricher to add the application version to all the log events. When you specify null, Raygun will use the assembly version.</param>
-    /// <param name="tags">Specifies the tags to include with every log message. The log level will always be included as a tag.</param>
-    /// <param name="ignoredFormFieldNames">Specifies the form field names which to ignore when including request form data.</param>
-    /// <param name="groupKeyProperty">The property containing the custom group key for the Raygun message.</param>
-    public RaygunSink(IFormatProvider formatProvider,
-        string applicationKey,
-        IEnumerable<Type> wrapperExceptions = null,
-        string userNameProperty = "UserName",
-        string applicationVersionProperty = "ApplicationVersion",
-        IEnumerable<string> tags = null,
-        IEnumerable<string> ignoredFormFieldNames = null,
-        string groupKeyProperty = null)
+    public class RaygunSink : ILogEventSink
     {
-      if (string.IsNullOrEmpty(applicationKey))
-        throw new ArgumentNullException("applicationKey");
+        readonly IFormatProvider _formatProvider;
+        readonly string _userNameProperty;
+        readonly string _applicationVersionProperty;
+        readonly IEnumerable<string> _tags;
+        readonly IEnumerable<string> _ignoredFormFieldNames;
+        readonly string _groupKeyProperty;
+        readonly RaygunClient _client;
 
-      _formatProvider = formatProvider;
-      _userNameProperty = userNameProperty;
-      _applicationVersionProperty = applicationVersionProperty;
-      _tags = tags ?? new string[0];
-      _ignoredFormFieldNames = ignoredFormFieldNames ?? Enumerable.Empty<string>();
-      _groupKeyProperty = groupKeyProperty;
+        /// <summary>
+        /// Grouping event delegate
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RaygunCustomGroupingKeyEventArgs"/> instance containing the event data.</param>
+        /// <param name="group">The custom group object containing the custom group key</param>
+        public delegate void Grouped(object sender, RaygunCustomGroupingKeyEventArgs e, IMessageGroup group);
 
-      _client = new RaygunClient(applicationKey);
-      if (wrapperExceptions != null)
-        _client.AddWrapperExceptions(wrapperExceptions.ToArray());
+        /// <summary>
+        /// Occurs when the Raygun client fires the CustomGroupKey event.
+        /// </summary>
+        public event Grouped Grouping;
+
+        /// <summary>
+        /// Construct a sink that saves errors to the Raygun.io service. Properties are being send as userdata and the level is included as tag. The message is included inside the userdata.
+        /// </summary>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="applicationKey">The application key as found on the Raygun website.</param>
+        /// <param name="wrapperExceptions">If you have common outer exceptions that wrap a valuable inner exception which you'd prefer to group by, you can specify these by providing a list.</param>
+        /// <param name="userNameProperty">Specifies the property name to read the username from. By default it is UserName. Set to null if you do not want to use this feature.</param>
+        /// <param name="applicationVersionProperty">Specifies the property to use to retrieve the application version from. You can use an enricher to add the application version to all the log events. When you specify null, Raygun will use the assembly version.</param>
+        /// <param name="tags">Specifies the tags to include with every log message. The log level will always be included as a tag.</param>
+        /// <param name="ignoredFormFieldNames">Specifies the form field names which to ignore when including request form data.</param>
+        /// <param name="groupKeyProperty">The property containing the custom group key for the Raygun message.</param>
+        public RaygunSink(IFormatProvider formatProvider,
+            string applicationKey,
+            IEnumerable<Type> wrapperExceptions = null,
+            string userNameProperty = "UserName",
+            string applicationVersionProperty = "ApplicationVersion",
+            IEnumerable<string> tags = null,
+            IEnumerable<string> ignoredFormFieldNames = null,
+            string groupKeyProperty = null)
+        {
+            if (string.IsNullOrEmpty(applicationKey))
+                throw new ArgumentNullException("applicationKey");
+
+            _formatProvider = formatProvider;
+            _userNameProperty = userNameProperty;
+            _applicationVersionProperty = applicationVersionProperty;
+            _tags = tags ?? new string[0];
+            _ignoredFormFieldNames = ignoredFormFieldNames ?? Enumerable.Empty<string>();
+            _groupKeyProperty = groupKeyProperty;
+
+            _client = new RaygunClient(applicationKey);
+            if (wrapperExceptions != null)
+                _client.AddWrapperExceptions(wrapperExceptions.ToArray());
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            //Include the log level as a tag.
+            var tags = _tags.Concat(new[] { logEvent.Level.ToString() }).ToList();
+
+            var properties = logEvent.Properties
+                         .Select(pv => new { Name = pv.Key, Value = RaygunPropertyFormatter.Simplify(pv.Value) })
+                         .ToDictionary(a => a.Name, b => b.Value);
+
+            // Add the message 
+            properties.Add("RenderedLogMessage", logEvent.RenderMessage(_formatProvider));
+            properties.Add("LogMessageTemplate", logEvent.MessageTemplate.Text);
+
+            // Wire up the CustomGroupingKey event if the custom group key property has been defined and has a non-null value
+            object customGroup;
+            if (!String.IsNullOrEmpty(_groupKeyProperty) && properties.TryGetValue(_groupKeyProperty, out customGroup) && !String.IsNullOrEmpty(customGroup.ToString()))
+                _client.CustomGroupingKey += (sender, args) => { OnGrouping(sender, args, new MessageGroup { GroupKey = customGroup.ToString() }); };
+
+            // Create new message
+            var raygunMessage = new RaygunMessage
+            {
+                OccurredOn = logEvent.Timestamp.UtcDateTime
+            };
+
+            // Add exception when available
+            if (logEvent.Exception != null)
+                raygunMessage.Details.Error = RaygunErrorMessageBuilder.Build(logEvent.Exception);
+
+            // Add user when requested
+            if (!String.IsNullOrWhiteSpace(_userNameProperty) &&
+                logEvent.Properties.ContainsKey(_userNameProperty) &&
+                logEvent.Properties[_userNameProperty] != null)
+            {
+                raygunMessage.Details.User = new RaygunIdentifierMessage(logEvent.Properties[_userNameProperty].ToString());
+            }
+
+            // Add version when requested
+            if (!String.IsNullOrWhiteSpace(_applicationVersionProperty) &&
+                logEvent.Properties.ContainsKey(_applicationVersionProperty) &&
+                logEvent.Properties[_applicationVersionProperty] != null)
+            {
+                raygunMessage.Details.Version = logEvent.Properties[_applicationVersionProperty].ToString();
+            }
+
+            // Build up the rest of the message
+            raygunMessage.Details.Environment = new RaygunEnvironmentMessage();
+            raygunMessage.Details.Tags = tags;
+            raygunMessage.Details.UserCustomData = properties;
+            raygunMessage.Details.MachineName = Environment.MachineName;
+
+            if (HttpContext.Current != null)
+            {
+                // Request message is built here instead of raygunClient.Send so RequestMessageOptions have to be constructed here
+                var requestMessageOptions = new RaygunRequestMessageOptions(_ignoredFormFieldNames, Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<string>());
+                raygunMessage.Details.Request = RaygunRequestMessageBuilder.Build(HttpContext.Current.Request, requestMessageOptions);
+            }
+
+            // Submit
+            _client.Send(raygunMessage);
+        }
+
+        /// <summary>
+        /// Even trigger for custom Grouping event
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RaygunCustomGroupingKeyEventArgs" /> instance containing the event data.</param>
+        /// <param name="group">The custom group object containing the key</param>
+        protected virtual void OnGrouping(object sender, RaygunCustomGroupingKeyEventArgs e, IMessageGroup group)
+        {
+            if (!String.IsNullOrEmpty(group.GroupKey))
+                e.CustomGroupingKey = group.GroupKey;
+
+            Grouping?.Invoke(this, e, group);
+        }
     }
-
-    /// <summary>
-    /// Emit the provided log event to the sink.
-    /// </summary>
-    /// <param name="logEvent">The log event to write.</param>
-    public void Emit(LogEvent logEvent)
-    {
-      //Include the log level as a tag.
-      var tags = _tags.Concat(new[] { logEvent.Level.ToString() }).ToList();
-
-      var properties = logEvent.Properties
-                   .Select(pv => new { Name = pv.Key, Value = RaygunPropertyFormatter.Simplify(pv.Value) })
-                   .ToDictionary(a => a.Name, b => b.Value);
-
-      // Add the message 
-      properties.Add("RenderedLogMessage", logEvent.RenderMessage(_formatProvider));
-      properties.Add("LogMessageTemplate", logEvent.MessageTemplate.Text);
-
-      // Wire up the CustomGroupingKey event if the custom group key property has been defined and has a non-null value
-      object customGroup;
-      if (!String.IsNullOrEmpty(_groupKeyProperty) && properties.TryGetValue(_groupKeyProperty, out customGroup) && !String.IsNullOrEmpty(customGroup.ToString()))
-        _client.CustomGroupingKey += (sender, args) => { OnGrouping(sender, args, new MessageGroup { GroupKey = customGroup.ToString()}); };
-
-      // Create new message
-      var raygunMessage = new RaygunMessage
-      {
-        OccurredOn = logEvent.Timestamp.UtcDateTime
-      };
-
-      // Add exception when available
-      if (logEvent.Exception != null)
-        raygunMessage.Details.Error = RaygunErrorMessageBuilder.Build(logEvent.Exception);
-
-      // Add user when requested
-      if (!String.IsNullOrWhiteSpace(_userNameProperty) &&
-          logEvent.Properties.ContainsKey(_userNameProperty) &&
-          logEvent.Properties[_userNameProperty] != null)
-      {
-        raygunMessage.Details.User = new RaygunIdentifierMessage(logEvent.Properties[_userNameProperty].ToString());
-      }
-
-      // Add version when requested
-      if (!String.IsNullOrWhiteSpace(_applicationVersionProperty) &&
-          logEvent.Properties.ContainsKey(_applicationVersionProperty) &&
-          logEvent.Properties[_applicationVersionProperty] != null)
-      {
-        raygunMessage.Details.Version = logEvent.Properties[_applicationVersionProperty].ToString();
-      }
-
-      // Build up the rest of the message
-      raygunMessage.Details.Environment = new RaygunEnvironmentMessage();
-      raygunMessage.Details.Tags = tags;
-      raygunMessage.Details.UserCustomData = properties;
-      raygunMessage.Details.MachineName = Environment.MachineName;
-     
-      if (HttpContext.Current != null)
-      {
-        // Request message is built here instead of raygunClient.Send so RequestMessageOptions have to be constructed here
-        var requestMessageOptions = new RaygunRequestMessageOptions(_ignoredFormFieldNames, Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<string>());
-        raygunMessage.Details.Request = RaygunRequestMessageBuilder.Build(HttpContext.Current.Request, requestMessageOptions);
-      }
-
-      // Submit
-      _client.Send(raygunMessage);
-    }
-
-    /// <summary>
-    /// Even trigger for custom Grouping event
-    /// </summary>
-    /// <param name="sender">The sender.</param>
-    /// <param name="e">The <see cref="RaygunCustomGroupingKeyEventArgs" /> instance containing the event data.</param>
-    /// <param name="group">The custom group object containing the key</param>
-    protected virtual void OnGrouping(object sender, RaygunCustomGroupingKeyEventArgs e, IMessageGroup group)
-    {
-      if (!String.IsNullOrEmpty(group.GroupKey))
-        e.CustomGroupingKey = group.GroupKey;
-
-      Grouping?.Invoke(this, e, group);  
-    }
-  }
 }
